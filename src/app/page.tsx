f179c5f512cc67aac804943e64a4c049d073878d
guimontico <guilherme.montico@townsq.com.br>
2023-08-22 19:03:42 -0300

    add avatar, dropdown, search and userNav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import NavBar from "@/components/NavBar";
-import Image from "next/image";
+import { Search } from "@/components/ui/search";
+import { UserNav } from "@/components/userNav";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-12">
       <div className="border-b">
-        <div className="flex h-16 items-center px-4">
+        <div className="flex justify-between w-screen h-16 items-center px-4">
           <NavBar />
-
           <div className="ml-auto flex items-center space-x-4">
-            {/* <Search />
-              <UserNav /> */}
+            <div className="hidden md:flex">
+              <Search />
+            </div>
+            <UserNav />
           </div>
         </div>
       </div>
